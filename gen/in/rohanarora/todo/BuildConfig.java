a3f92dd217d3de9e519203ca7f4d66ff802732b0
RISHABH SHARMA <rishabhsharma@ufl.edu>
2017-03-17 14:59:03 -0400

    Update BuildConfig.java

--- gen/in/rohanarora/todo/BuildConfig.java
+++ gen/in/rohanarora/todo/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package in.rohanarora.todo;
+package in.rishabhsharma.todo;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
